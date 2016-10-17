fd0f875c144bcce7218fe41fba69da7ed26f3e3d
HZWYL <952942236@qq.com>
2016-10-17 17:05:51 +0800

    添加支付宝，测试环境有待验证

---
+++ MYChat/Pay/PayManager.h
@@ -0,0 +1,17 @@
+//
+//  PayManager.h
+//  MYChat
+//
+//  Created by ycd15 on 16/10/17.
+//  Copyright © 2016年 YCD_WYL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <AlipaySDK/AlipaySDK.h>
+
+@interface PayManager : NSObject
+
+
+
+@end
---
+++ MYChat/Pay/PayManager.m
@@ -0,0 +1,13 @@
+//
+//  PayManager.m
+//  MYChat
+//
+//  Created by ycd15 on 16/10/17.
+//  Copyright © 2016年 YCD_WYL. All rights reserved.
+//
+
+#import "PayManager.h"
+
+@implementation PayManager
+
+@end
